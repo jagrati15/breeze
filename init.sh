8b62536e556f1828f3659b4c2624339c20575b04
jagrati15 <59571214+jagrati15@users.noreply.github.com>
2020-12-08 14:14:51 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -26,12 +26,12 @@ elastic_cloud_version=1.3.0
 elastic_stack_version=7.10.0
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
-mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
-mv ${path}/docker-playbook/version ${path}/docker-playbook/${docker_version}-CE
-mv ${path}/loadbalancer-playbook/version ${path}/loadbalancer-playbook/${loadbalancer_version}
-mv ${path}/prometheus-playbook/version ${path}/prometheus-playbook/Kube-Prometheus-v${kube_prometheus_version}
-mv ${path}/istio-playbook/version ${path}/istio-playbook/v${istio_version}
-mv ${path}/elasticcloud-playbook/version ${path}/elasticcloud-playbook/v${elastic_cloud_version}
+#mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
+#mv ${path}/docker-playbook/version ${path}/docker-playbook/${docker_version}-CE
+#mv ${path}/loadbalancer-playbook/version ${path}/loadbalancer-playbook/${loadbalancer_version}
+#mv ${path}/prometheus-playbook/version ${path}/prometheus-playbook/Kube-Prometheus-v${kube_prometheus_version}
+#mv ${path}/istio-playbook/version ${path}/istio-playbook/v${istio_version}
+#mv ${path}/elasticcloud-playbook/version ${path}/elasticcloud-playbook/v${elastic_cloud_version}
 
 #docker run --rm --name=kubeadm-version sakshi9715/kubeadm-version:v${kubernetes_version} kubeadm config images list --kubernetes-version ${kubernetes_version} > ${path}/k8s-images-list.txt
 docker run --rm --name=kubeadm-version ab1997/local:v${kubernetes_version} kubeadm config images list --kubernetes-version ${kubernetes_version} > ${path}/k8s-images-list.txt
@@ -42,24 +42,24 @@ mv etcd-playbook/version-by-kubeadm etcd-playbook/${etcd_version}
 
 echo "ETCD Version: ${etcd_version}" > ${path}/components-version.txt
 echo "Kubernetes Version: ${kubernetes_version}" >> ${path}/components-version.txt
-echo "Harbor Version: ${harbor_version}" >> ${path}/components-version.txt
-echo "Docker Version: ${docker_version}" >> ${path}/components-version.txt
-echo "HAProxy Version: ${haproxy_version}" >> ${path}/components-version.txt
-echo "Keepalived Version: ${keepalived_version}" >> ${path}/components-version.txt
-echo "Prometheus Version: ${prometheus_version}" >> ${path}/components-version.txt
-echo "PrometheusOperator Version: ${prometheus_operator_version}" >> ${path}/components-version.txt
-echo "KubePrometheus Version: ${kube_prometheus_version}" >> ${path}/components-version.txt
-echo "MetricsServer Version: ${metrics_server_version}" >> ${path}/components-version.txt
-echo "Dashboard Version: ${dashboard_version}" >> ${path}/components-version.txt
-echo "MetricsScraper Version: ${metrics_scraper_version}" >> ${path}/components-version.txt
-echo "Flannel Version: ${flannel_version}" >> ${path}/components-version.txt
-echo "Calico Version: ${calico_version}" >> ${path}/components-version.txt
-echo "Helm Version: ${helm_version}" >> ${path}/components-version.txt
-echo "Istio Version: ${istio_version}" >> ${path}/components-version.txt
-echo "Contour Version: ${contour_version}" >> ${path}/components-version.txt
-echo "ContourEnvoyProxy Version: ${contour_envoyproxy_version}" >> ${path}/components-version.txt
-echo "ElasticCloud Version: ${elastic_cloud_version}" >> ${path}/components-version.txt
-echo "ElasticStack Version: ${elastic_stack_version}" >> ${path}/components-version.txt
+#echo "Harbor Version: ${harbor_version}" >> ${path}/components-version.txt
+#echo "Docker Version: ${docker_version}" >> ${path}/components-version.txt
+#echo "HAProxy Version: ${haproxy_version}" >> ${path}/components-version.txt
+#echo "Keepalived Version: ${keepalived_version}" >> ${path}/components-version.txt
+#echo "Prometheus Version: ${prometheus_version}" >> ${path}/components-version.txt
+#echo "PrometheusOperator Version: ${prometheus_operator_version}" >> ${path}/components-version.txt
+#echo "KubePrometheus Version: ${kube_prometheus_version}" >> ${path}/components-version.txt
+#echo "MetricsServer Version: ${metrics_server_version}" >> ${path}/components-version.txt
+#echo "Dashboard Version: ${dashboard_version}" >> ${path}/components-version.txt
+#echo "MetricsScraper Version: ${metrics_scraper_version}" >> ${path}/components-version.txt
+#echo "Flannel Version: ${flannel_version}" >> ${path}/components-version.txt
+#echo "Calico Version: ${calico_version}" >> ${path}/components-version.txt
+#echo "Helm Version: ${helm_version}" >> ${path}/components-version.txt
+#echo "Istio Version: ${istio_version}" >> ${path}/components-version.txt
+#echo "Contour Version: ${contour_version}" >> ${path}/components-version.txt
+#echo "ContourEnvoyProxy Version: ${contour_envoyproxy_version}" >> ${path}/components-version.txt
+#echo "ElasticCloud Version: ${elastic_cloud_version}" >> ${path}/components-version.txt
+#echo "ElasticStack Version: ${elastic_stack_version}" >> ${path}/components-version.txt
 
 for dir in `ls ${path}`
 do
