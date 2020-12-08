9433b5314dda9406c8ef4b8ade57b7ac59348a8a
jagrati15 <59571214+jagrati15@users.noreply.github.com>
2020-12-08 15:08:55 +0530

    Update init.sh

--- etcd-playbook/version-by-kubeadm/init.sh
+++ etcd-playbook/version-by-kubeadm/init.sh
@@ -16,8 +16,21 @@ docker save ${image} > ${path}/file/etcd.tar
 bzip2 -z --best ${path}/file/etcd.tar
 
 echo "===xxxxxxxxxxxxx download cfssl toolsxxxxxxxxxxxxxxxxxxxx ==="
-if [[ $$(uname -m) == "aarch64" ]]; then
+if [[ $(uname -m) == "aarch64" ]]; then
   echo "ARM64 doneeeeeee"
+  wget https://dl.google.com/go/go1.12.linux-arm64.tar.gz
+  tar -xf go1.12.linux-arm64.tar.gz
+  export GOROOT=/usr/local/go
+  pwd
+  export GOPATH=
+  export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+  git clone https://github.com/cloudflare/cfssl
+  cd cfssl
+  make install && cd bin
+  chmod +x cfssl cfssljson cfssl-certinfo
+  tar zcvf ${path}/file/cfssl-tools.tar.gz cfssl cfssl-certinfo cfssljson
+  cd ../..
+  rm -rf cfssl
 else
   echo "AMD64 doneeeee"
 fi
